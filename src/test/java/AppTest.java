f00cd4a78f0670e84ddf86a94f22416096e70a21
YAWEI <almostloverv@yahoo.com>
2018-02-23 15:32:50 +0700

    add more detail to main and test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,8 +5,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    @Test
+    public void testSmallPrime(){
+        App appObj = new App();
+        assertEquals(appObj.isPrime(17),true);
+    }
+
+    @Test
+    public void testSmallNoprime(){
+        App appObj = new App();
+        assertEquals(appObj.isPrime(33),false);
+    }
+
+    @Test
+    public void testLargePrime(){
+        App appObj = new App();
+        assertEquals(appObj.isPrime(32416189049L),true);
+    }
+
+    @Test
+    public void testLargeNoPrime(){
+        App appObj =new App();
+        assertEquals(appObj.isPrime(32416189051L),false);
     }
 }
